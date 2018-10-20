fc8c880e54cc59fe00209de8332cdec1e37becfc
Brighton Tanvir <brightontanvir@gmail.com>
2018-10-20 16:08:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,9 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
+ function accessElementInArray(array, index) {
+  return array[index]
+}
