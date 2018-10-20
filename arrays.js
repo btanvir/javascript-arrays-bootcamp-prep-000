8dabb350b0aee9e7860642ee4f5d5ffea21a06d7
Brighton Tanvir <brightontanvir@gmail.com>
2018-10-20 16:06:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,4 +21,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array
 }
-function removeElementFromBeginningOfArray(array)
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
